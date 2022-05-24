789fc58bb028dd54a7231b3fd6207b5966c5c8ac
a201106 <77398406+a201106@users.noreply.github.com>
2022-05-24 16:46:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+
+leftWristX = 0;
+rightWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -13,12 +20,24 @@ function modelLoaded(){
     console.log("PoseNet is initialised");
 }
 
-function draw() {
-    background("#293d3d");
+function gotPoses(results) {
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "  noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + "  rightWristX = " + rightWristX);
+    }
 }
 
-function gotPoses() {
-    if(results.length > 0){
-        console.log(results)
-    }
+function draw() {
+    background("#293d3d");
+    document.getElementById("font_size").innerHTML = "Font size = " + difference + "px";
+    fill('#04ECF7');
+    stroke('#000000');
+    text('Aaron', noseX, noseY);
+    textSize(difference);
 }
